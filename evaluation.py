74325a0e326871b1481175ceb7436c64cb7b5135
Mary Nansikombi <107887740+nansikom@users.noreply.github.com>
2025-08-27 12:25:36 -0700

    Obscure API key in evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -30,7 +30,7 @@ def generate_evaluation(result):
     """
     
     
-    client = genai.Client(api_key="AIzaSyDf987k_HpOVlnpddR3-Z4Ybxr8nP5cB0w") 
+    client = genai.Client(api_key="----") 
 
     response = client.models.generate_content(
     model="gemini-2.0-flash", contents= prompt
